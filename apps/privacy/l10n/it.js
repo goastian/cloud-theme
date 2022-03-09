78445ff1bb4e2ac39e7888c101eae4e91c502471
Avinash Gusain <avinashg@e.email>
2022-03-09 22:50:51 +0530

    new translation added for privacy to handle who can access section

---
+++ apps/privacy/l10n/it.js
@@ -0,0 +1,7 @@
+OC.L10N.register(
+    "privacy",
+    {
+    "Who can access main text":"Like in most cloud services, administrators can see your files and all the information in the database as they need to make backups, perform upgrades, reset passwords, etc.",
+    "Who can access main text link":"Learn more about this topic here.",
+},
+"nplurals=2; plural=(n != 1);");
