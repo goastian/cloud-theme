4435a9013e82fbf7518277323c798481e63c03e8
AVINASH GUSAIN <avinashg@e.email>
2022-03-14 20:50:18 +0000

    new translation added for privacy to handle who can access section

---
+++ apps/privacy/l10n/de.js
@@ -0,0 +1,7 @@
+OC.L10N.register(
+    "privacy",
+    {
+        "Like in most cloud services, administrators can see your files and all the information in the database as they need to make backups, perform upgrades, reset passwords, etc.": "Like in most cloud services, administrators can see your files and all the information in the database as they need to make backups, perform upgrades, reset passwords, etc.",
+        "Learn more about this topic here.": "Learn more about this topic here.",
+    },
+    "nplurals=2; plural=(n != 1);");
---
+++ apps/privacy/l10n/fr.js
@@ -0,0 +1,7 @@
+OC.L10N.register(
+    "privacy",
+    {
+        "Like in most cloud services, administrators can see your files and all the information in the database as they need to make backups, perform upgrades, reset passwords, etc.": "Comme chez la plupart des services cloud, les administrateurs peuvent voir vos fichiers et toutes les informations en base de données car ils ont besoin de pouvoir effectuer des sauvegardes, réinitialiser des mots de passe, etc.",
+        "Learn more about this topic here.": "Pour en savoir plus, cliquez ici.",
+    },
+    "nplurals=2; plural=(n != 1);");
