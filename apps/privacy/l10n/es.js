92ffc7f01374f5f0abbc817ae7619697b3cf5e2b
Avinash Gusain <avinashg@e.email>
2022-03-15 20:28:55 +0530

    translation modified for privacy

--- apps/privacy/l10n/es.js
+++ apps/privacy/l10n/es.js
@@ -1,7 +1,12 @@
 OC.L10N.register(
     "privacy",
     {
-        "Like in most cloud services, administrators can see your files and all the information in the database as they need to make backups, perform upgrades, reset passwords, etc.": "Like in most cloud services, administrators can see your files and all the information in the database as they need to make backups, perform upgrades, reset passwords, etc.",
-        "Learn more about this topic here.": "Learn more about this topic here.",
+        "Like in most cloud services, a reduced number of administrators can see your files and all the information in the database as they need to make backups, perform upgrades, reset passwords, etc.": "Like in most cloud services, a reduced number of administrators can see your files and all the information in the database as they need to make backups, perform upgrades, reset passwords, etc.",
+        "Learn more about this topic": "Learn more about this topic",
+        "To protect your data, we have implemented server-side-encryption on our servers which hides by default the content of your files and notes even to these administrators.": "To protect your data, we have implemented server-side-encryption on our servers which hides by default the content of your files and notes even to these administrators.",
+        "People you shared documents or folders with": "People you shared documents or folders with",
+        "Where is my data?": "Where is my data?",
+        "Who has access to my data?": "Who has access to my data?"
     },
-    "nplurals=2; plural=(n != 1);");
+    "nplurals=2; plural=(n != 1);"
+);
