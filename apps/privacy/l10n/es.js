c9c17d80d4585c643534fa6d8dd87981933f03db
Avinash Gusain <avinashg@e.email>
2022-03-16 11:59:19 +0530

    all translations added

--- apps/privacy/l10n/es.js
+++ apps/privacy/l10n/es.js
@@ -1,7 +1,13 @@
 OC.L10N.register(
     "privacy",
     {
-        "Like in most cloud services, administrators can see your files and all the information in the database as they need to make backups, perform upgrades, reset passwords, etc.": "Como en la mayoría de los servicios en la nube, los administradores pueden ver los ficheros y toda la información en la base de datos; de esta manera pueden hacer copias de respaldo, realizar actualizaciones o reiniciar contraseñas, entre otras operaciones.",
-        "Learn more about this topic here.": "Lea más sobre el tema aquí.",
+        "Like in most cloud services, a reduced number of administrators can see your files and all the information in the database as they need to make backups, perform upgrades, reset passwords, etc.": "Como en la mayoría de los servicios en la nube, un número reducido de administradores pueden ver los ficheros y toda la información en la base de datos; de esta manera pueden hacer copias de respaldo, realizar actualizaciones o reiniciar contraseñas, entre otras operaciones.",
+        "Learn more about this topic": "Lea más sobre el tema",
+        "here":"aquí",
+        "To protect your data, we have implemented server-side-encryption on our servers which hides by default the content of your files and notes even to these administrators.": "Para proteger tus datos, hemos activado el cifrado en el servidor que impide por defecto ver el contenido de los ficheros y notas, incluso a los administradores.",
+        "People you shared documents or folders with": "Personen, für die Sie Dokumente oder Ordner freigegeben haben:",
+        "Where is my data?": "¿Dónde están almacenados mis datos?",
+        "Who has access to my data?": "¿Quién puede acceder a mis datos?"
     },
-    "nplurals=2; plural=(n != 1);");
+    "nplurals=2; plural=(n != 1);"
+);
