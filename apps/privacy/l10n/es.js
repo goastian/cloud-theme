4aa2c46a6978e28140ad60a671d18575656a95ce
Avinash Gusain <avinashg@e.email>
2022-03-17 12:48:48 +0530

    final fixes in translation for who has access privacy section

--- apps/privacy/l10n/es.js
+++ apps/privacy/l10n/es.js
@@ -2,8 +2,7 @@ OC.L10N.register(
     "privacy",
     {
         "Like in most cloud services, a reduced number of administrators can see your files and all the information in the database as they need to make backups, perform upgrades, reset passwords, etc.": "Como en la mayoría de los servicios en la nube, un número reducido de administradores pueden ver los ficheros y toda la información en la base de datos; de esta manera pueden hacer copias de respaldo, realizar actualizaciones o reiniciar contraseñas, entre otras operaciones.",
-        "Learn more about this topic": "Lea más sobre el tema",
-        "here":"aquí",
+        "Learn more about this topic here.": "Lea más sobre el tema aquí.",
         "To protect your data, we have implemented server-side-encryption on our servers which hides by default the content of your files and notes even to these administrators.": "Para proteger tus datos, hemos activado el cifrado en el servidor que impide por defecto ver el contenido de los ficheros y notas, incluso a los administradores.",
         "People you shared documents or folders with": "Personen, für die Sie Dokumente oder Ordner freigegeben haben:",
         "Where is your data?": "¿Dónde están almacenados mis datos?",
