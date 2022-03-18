6cd0bbc22b61d645bbf2885bca47502abad34686
Arnau Vàzquez <arnauvp@e.email>
2022-03-18 10:27:55 +0000

    chez -> pour

--- apps/privacy/l10n/fr.js
+++ apps/privacy/l10n/fr.js
@@ -1,7 +1,7 @@
 OC.L10N.register(
     "privacy",
     {
-        "Like in most cloud services, a reduced number of administrators can see your files and all the information in the database as they need to make backups, perform upgrades, reset passwords, etc.": "Comme chez la plupart des services cloud, un nombre réduit d'administrateurs peuvent voir vos fichiers et toutes les informations en base de données car ils ont besoin de pouvoir effectuer des sauvegardes, réinitialiser des mots de passe, etc.",
+        "Like in most cloud services, a reduced number of administrators can see your files and all the information in the database as they need to make backups, perform upgrades, reset passwords, etc.": "Comme pour la plupart des services cloud, un nombre réduit d'administrateurs peuvent voir vos fichiers et toutes les informations en base de données car ils ont besoin de pouvoir effectuer des sauvegardes, réinitialiser des mots de passe, etc.",
         "Learn more about this topic here.": "Pour en savoir plus, cliquez ici.",
         "To protect your data, we have implemented server-side-encryption on our servers which hides by default the content of your files and notes even to these administrators.": "Pour protéger vos données, nous avons implémenté un chiffrement côté serveurs qui dissimule par défaut le contenu de vos fichiers et notes, même à ces administrateurs.",
         "People you shared documents or folders with": "Les personnes avec lesquelles vous avez partagé des fichiers ou dossiers",
