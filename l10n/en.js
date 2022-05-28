0000b04e7c886ffc98249f7068035cd53ea51eba
Arnau Vàzquez <arnauvp@e.email>
2022-05-28 02:47:31 +0000

    Dev/login page redesign

---
+++ l10n/en.js
@@ -0,0 +1,15 @@
+OC.L10N.register(
+    "core",
+    {
+		"Get your FREE Murena account now" : "Get your FREE Murena account now",
+		"1GB storage for FREE to store and sync your pictures & videos.": "1GB storage for FREE to store and sync your pictures & videos.",
+		"Edit your documents online.": "Edit your documents online.",
+		"Your unique email address @murena.io": "Your unique email address @murena.io",
+		"Sync calendar and contacts with the cloud": "Sync calendar and contacts with the cloud",
+		"and many new features added regularly!": "and many new features added regularly!",
+		"Create My Account": "Create My Account",
+		"Sign in to your account": "Sign in to your account",
+		"Terms & Conditions": "Terms & Conditions",
+		"Privacy Policy": "Privacy Policy"
+},
+"nplurals=2; plural=(n != 1);");
