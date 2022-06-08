b50c840f1f582fab5f7026f6a2a9dfd82c207351
Arnau Vàzquez <arnauvp@e.email>
2022-06-08 13:49:46 +0000

    Small adjustments to ES login page copy

--- core/l10n/es.js
+++ core/l10n/es.js
@@ -1,14 +1,14 @@
 OC.L10N.register(
     "core",
     {
-		"Get your FREE Murena account now" : "Obtenga su cuenta Murena GRATIS ahora",
+		"Get your FREE Murena account now" : "Obtén tu cuenta Murena GRATIS ahora",
 		"1GB storage for FREE to store and sync your pictures & videos.": "1 GB de almacenamiento GRATIS para almacenar y sincronizar tus fotos y videos.",
-		"Edit your documents online.": "Edite sus documentos en línea.",
-		"Your unique email address @murena.io": "Su dirección de correo electrónico única @murena.io",
+		"Edit your documents online.": "Edita tus documentos en línea.",
+		"Your unique email address @murena.io": "Tu dirección de correo electrónico única @murena.io",
 		"Sync calendar and contacts with the cloud": "Sincroniza calendario y contactos con la nube",
 		"and many new features added regularly!": "¡y muchas características nuevas agregadas regularmente!",
 		"Create My Account": "Crear mi cuenta",
-		"Sign in to your account": "Iniciar sesión en su cuenta",
+		"Sign in to your account": "Iniciar sesión en tu cuenta",
 		"Terms & Conditions": "Términos y condiciones",
 		"Privacy Policy": "Política de privacidad",
 		"All Rights Reserved": "Reservados todos los derechos",
