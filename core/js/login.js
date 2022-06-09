7423a07c349574c62ec909b9369ae7300d3a6808
Ronak Patel <ronak.patel@e.email>
2022-06-09 21:06:58 +0530

    change in login js

--- core/js/login.js
+++ core/js/login.js
@@ -1,12 +1,10 @@
 $(document).on('click','#lost-password', function(){
-	$('.banner-right-align').hide(500);
+	$('.banner-right-align').addClass('width0');
+	$('.lines').hide();
 	$('.v-align').addClass('warning-messsage');
-	$(".v-align").fadeOut('fast').delay(300).fadeIn('500');
-	$('.fp-label').fadeIn(); $('.sign-label').fadeOut('fast');
 });
 $(document).on('click','.lost-password-container a', function(){
-	$('.banner-right-align').show(500);
+	$('.banner-right-align').removeClass('width0');
+	$('.lines').show();
 	$('.v-align').removeClass('warning-messsage');
-	$(".v-align").fadeOut('fast').delay(300).fadeIn('500');
-	$('.fp-label').fadeOut('fast'); $('.sign-label').fadeIn();
 });
