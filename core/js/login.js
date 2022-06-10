14c888ee3073f733ae688c9330255f27696652c0
Ronak Patel <ronak.patel@e.email>
2022-06-10 13:24:15 +0530

    login transition

--- core/js/login.js
+++ core/js/login.js
@@ -1,10 +1,12 @@
 $(document).on('click','#lost-password', function(){
+	$('.banner-content').hide('fast');
 	$('.banner-right-align').addClass('width0');
 	$('.lines').hide();
 	$('.v-align').addClass('warning-messsage');
 });
 $(document).on('click','.lost-password-container a', function(){
 	$('.banner-right-align').removeClass('width0');
-	$('.lines').show();
 	$('.v-align').removeClass('warning-messsage');
+	$('.lines').show('slow');
+	$('.banner-content').show('slow');
 });
