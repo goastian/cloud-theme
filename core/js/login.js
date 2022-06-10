5b22a35fe2a37f4e8918ccd00d856ebb65e59f87
Ronak P Patel <ronak.patel@e.email>
2022-06-10 17:24:55 +0000

    login responsive issue and new JS file for `CSR` issue

---
+++ core/js/login.js
@@ -0,0 +1,12 @@
+$(document).on('click','#lost-password', function(){
+	$('.banner-content').hide('slow');
+	$('.banner-right-align').addClass('width0');
+	$('.lines').hide();
+	$('.v-align').addClass('warning-messsage');
+});
+$(document).on('click','.lost-password-container a', function(){
+	$('.banner-right-align').removeClass('width0');
+	$('.v-align').removeClass('warning-messsage');
+	$('.lines').show('slow');
+	$('.banner-content').show('slow');
+});
