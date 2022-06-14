22d4290c736dadf0e96782a334cd0375b580d9d9
Ronak Patel <ronak.patel@e.email>
2022-06-14 14:38:50 +0530

    css animation added on footer links

--- core/js/login.js
+++ core/js/login.js
@@ -3,10 +3,12 @@ $(document).on('click','#lost-password', function(){
 	$('.banner-right-align').addClass('width0');
 	$('.lines').hide();
 	$('.v-align').addClass('warning-messsage');
+	$('footer').addClass('forgotpass-footer');
 });
 $(document).on('click','.lost-password-container a', function(){
 	$('.banner-right-align').removeClass('width0');
 	$('.v-align').removeClass('warning-messsage');
+	$('footer').removeClass('forgotpass-footer');
 	$('.lines').show('slow');
 	$('.banner-content').show('slow');
 });
