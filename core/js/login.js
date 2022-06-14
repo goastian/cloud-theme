6efe9b093d28471a7110934aed5b80edd2da09b5
Ronak Patel <ronak.patel@e.email>
2022-06-14 14:48:53 +0530

    added condition for big screen for js

--- core/js/login.js
+++ core/js/login.js
@@ -1,14 +1,18 @@
 $(document).on('click','#lost-password', function(){
-	$('.banner-content').hide('slow');
-	$('.banner-right-align').addClass('width0');
-	$('.lines').hide();
-	$('.v-align').addClass('warning-messsage');
-	$('footer').addClass('forgotpass-footer');
+	if(window.innerWidth > 1050){
+		$('.banner-content').hide('slow');
+		$('.banner-right-align').addClass('width0');
+		$('.lines').hide();
+		$('.v-align').addClass('warning-messsage');
+		$('footer').addClass('forgotpass-footer');
+	}
 });
 $(document).on('click','.lost-password-container a', function(){
-	$('.banner-right-align').removeClass('width0');
-	$('.v-align').removeClass('warning-messsage');
-	$('footer').removeClass('forgotpass-footer');
-	$('.lines').show('slow');
-	$('.banner-content').show('slow');
+	if(window.innerWidth > 1050){
+		$('.banner-right-align').removeClass('width0');
+		$('.v-align').removeClass('warning-messsage');
+		$('footer').removeClass('forgotpass-footer');
+		$('.lines').show('slow');
+		$('.banner-content').show('slow');
+	}
 });
