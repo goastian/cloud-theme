66b3b2370f38038dd91211c625d64e64c4d82dfa
Ronak Patel <ronak.patel@e.email>
2022-07-27 18:47:00 +0530

    added translations for 2FA-Note #2

--- core/l10n/fr.js
+++ core/l10n/fr.js
@@ -15,6 +15,6 @@ OC.L10N.register(
 		"Copyright": "droits d'auteur",
 		"Don't have an account yet?": "Vous n'avez pas encore de compte ?",
 		"Create an account": "Créer un compte",
-		"2FA-Note": "<b>Remarque:</b> Vous devrez créer un nouveau mot de <a class=\"text-color-007fff\" href=\"https://doc.e.foundation/support-topics/two-factor-authentication#using-an-application-password-for-the-eos-account-manager\">passe d'application</a> pour configurer votre compte dans /e/OS. En outre, vous devez <a class=\"text-color-007fff\" href=\"https://doc.e.foundation/support-topics/two-factor-authentication#manually-setup-mail-application\">fournir le mot de passe de votre compte principal à /e/OS Mail.</a>"
+		"2FA-Note": "<b>Remarque:</b> Vous devrez créer un nouveau mot de <a class=\"text-color-007fff\" href=\"https://doc.e.foundation/support-topics/two-factor-authentication#using-an-application-password-for-the-eos-account-manager\">passe d'application</a> pour configurer votre compte dans /e/OS. En outre, vous devez <a class=\"text-color-007fff\" href=\"https://doc.e.foundation/support-topics/two-factor-authentication#manually-setup-mail-application\">fournir le mot de passe de votre compte principal à /e/OS Mail</a>."
 },
 "nplurals=2; plural=(n != 1);");
