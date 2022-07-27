b44e07940245f235cdf4cb5d9575e938f21df435
Ronak Patel <ronak.patel@e.email>
2022-07-27 18:45:07 +0530

    added translations for 2FA-Note

--- core/l10n/en.js
+++ core/l10n/en.js
@@ -14,6 +14,7 @@ OC.L10N.register(
 		"All Rights Reserved": "All Rights Reserved",
 		"Copyright": "Copyright",
 		"Don't have an account yet?": "Don't have an account yet?",
-		"Create an account": "Create an account"
+		"Create an account": "Create an account",
+		"2FA-Note": "<b>Note:</b> You'll need to create a new <a class=\"text-color-007fff\" href=\"https://doc.e.foundation/support-topics/two-factor-authentication#using-an-application-password-for-the-eos-account-manager\">application password</a> to set your account within /e/OS. Additionally, you need to <a class=\"text-color-007fff\" href=\"https://doc.e.foundation/support-topics/two-factor-authentication#manually-setup-mail-application\">provide your main account password to /e/OS Mail</a>"
 },
 "nplurals=2; plural=(n != 1);");
