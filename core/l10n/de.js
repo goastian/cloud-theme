07685c821738844e0ca27df799bf3d44fd5c49e4
Ronak Patel <ronak.patel@e.email>
2022-07-28 12:41:38 +0530

    renamed class name

--- core/l10n/de.js
+++ core/l10n/de.js
@@ -15,6 +15,6 @@ OC.L10N.register(
 		"Copyright": "Auteursrechten",
 		"Don't have an account yet?": "Heb je nog geen account?",
 		"Create an account": "Account aanmaken",
-		"2FA-Note": "<b>Notiz:</b> Sie müssen ein neues <a class=\"text-color-007fff\" href=\"https://doc.e.foundation/support-topics/two-factor-authentication#using-an-application-password-for-the-eos-account-manager\">anwendungs ​​kennwort</a> erstellen um Ihr Konto in /e/OS einzurichten. Zusätzlich müssen Sie Ihr <a class=\"text-color-007fff\" href=\"https://doc.e.foundation/support-topics/two-factor-authentication#manually-setup-mail-application\">Hauptkonto-Passwort für /e/OS Mail angeben</a>."
+		"2FA-Note": "<b>Notiz:</b> Sie müssen ein neues <a class=\"text-color-active\" href=\"https://doc.e.foundation/support-topics/two-factor-authentication#using-an-application-password-for-the-eos-account-manager\">anwendungs ​​kennwort</a> erstellen um Ihr Konto in /e/OS einzurichten. Zusätzlich müssen Sie Ihr <a class=\"text-color-active\" href=\"https://doc.e.foundation/support-topics/two-factor-authentication#manually-setup-mail-application\">Hauptkonto-Passwort für /e/OS Mail angeben</a>."
 },
 "nplurals=2; plural=(n != 1);");
--- core/l10n/es.js
+++ core/l10n/es.js
@@ -15,6 +15,6 @@ OC.L10N.register(
 		"Copyright": "Derechos de autor",
 		"Don't have an account yet?": "¿Aún no tienes una cuenta?",
 		"Create an account": "Regístrate!",
-		"2FA-Note": "<b>Nota:</b> Deberá crear una nueva <a class=\"text-color-007fff\" href=\"https://doc.e.foundation/support-topics/two-factor-authentication#using-an-application-password-for-the-eos-account-manager\">contraseña de aplicación</a> para configurar su cuenta dentro de /e/OS. Además, debe proporcionar <a class=\"text-color-007fff\" href=\"https://doc.e.foundation/support-topics/two-factor-authentication#manually-setup-mail-application\">la contraseña de su cuenta principal a /e/OS Mail</a>."
+		"2FA-Note": "<b>Nota:</b> Deberá crear una nueva <a class=\"text-color-active\" href=\"https://doc.e.foundation/support-topics/two-factor-authentication#using-an-application-password-for-the-eos-account-manager\">contraseña de aplicación</a> para configurar su cuenta dentro de /e/OS. Además, debe proporcionar <a class=\"text-color-active\" href=\"https://doc.e.foundation/support-topics/two-factor-authentication#manually-setup-mail-application\">la contraseña de su cuenta principal a /e/OS Mail</a>."
 },
 "nplurals=2; plural=(n != 1);");
