bfdbb26bda8ad725476e27b94251546ede3445b8
Ronak P Patel <ronak.patel@e.email>
2022-07-28 08:17:16 +0000

    Text Color and Translations for 2FA notes

---
+++ apps/settings/l10n/es.js
@@ -0,0 +1,6 @@
+OC.L10N.register(
+    "settings",
+    {
+		"2FA-Note": "<b>Nota:</b> Deberá crear una nueva <a class=\"text-color-active\" href=\"https://doc.e.foundation/support-topics/two-factor-authentication#using-an-application-password-for-the-eos-account-manager\">contraseña de aplicación</a> para configurar su cuenta dentro de /e/OS. Además, debe proporcionar <a class=\"text-color-active\" href=\"https://doc.e.foundation/support-topics/two-factor-authentication#manually-setup-mail-application\">la contraseña de su cuenta principal a /e/OS Mail</a>."
+},
+"nplurals=2; plural=(n != 1);");
---
+++ apps/settings/l10n/fr.js
@@ -0,0 +1,6 @@
+OC.L10N.register(
+	"settings",
+    {
+		"2FA-Note": "<b>Remarque:</b> Vous devrez créer un nouveau mot de <a class=\"text-color-active\" href=\"https://doc.e.foundation/support-topics/two-factor-authentication#using-an-application-password-for-the-eos-account-manager\">passe d'application</a> pour configurer votre compte dans /e/OS. En outre, vous devez <a class=\"text-color-active\" href=\"https://doc.e.foundation/support-topics/two-factor-authentication#manually-setup-mail-application\">fournir le mot de passe de votre compte principal à /e/OS Mail</a>."
+},
+"nplurals=2; plural=(n != 1);");
