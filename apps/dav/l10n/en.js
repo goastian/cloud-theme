1cf43327152f8b6362ffe2572405146edd9db2f6
Arnau Vàzquez <arnauvp@e.email>
2022-08-10 15:25:37 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- apps/dav/l10n/en.js
+++ apps/dav/l10n/en.js
@@ -5,7 +5,7 @@ OC.L10N.register(
         "This occurrence has been updated, please review highlighted information below:":"This occurrence has been updated, please review highlighted information below:",
         "This event has been updated, please review the information below:":"This event has been updated, please review the information below:",
         "This recurring event has been updated, please review the information below:":"This recurring event has been updated, please review the information below:",
-        "The invitation has been cancelled":"The invitation has been cancelled"
+        "The invitation has been cancelled":"The event has been cancelled"
     },
     "nplurals=2; plural=(n != 1);"
 );
