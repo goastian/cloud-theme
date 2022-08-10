c75ffcba310bd768cccbb4642a6a347f48e18c82
Avinash Gusain <avinashg@e.email>
2022-08-10 16:34:43 +0530

    transalation correction

--- apps/dav/l10n/de_DE.js
+++ apps/dav/l10n/de_DE.js
@@ -4,7 +4,7 @@ OC.L10N.register(
        "Maybe": "Vielleicht",
        "This occurrence has been updated, please review highlighted information below:":"Dieses Ereignis wurde aktualisiert, bitte überprüfen Sie die hervorgehobenen Informationen unten:",
        "This event has been updated, please review the information below:":"Diese Veranstaltung wurde aktualisiert, bitte beachten Sie die unten stehenden Informationen:",
-       "This recurring event has been updated, please review the information below:":" Diese wiederkehrende Veranstaltung wurde aktualisiert, bitte beachten Sie die unten stehenden Informationen:",
+       "This recurring event has been updated, please review the information below:":"Diese wiederkehrende Veranstaltung wurde aktualisiert, bitte beachten Sie die unten stehenden Informationen:",
        "The invitation has been cancelled":"Die Veranstaltung wurde abgesagt"
     },
     "nplurals=2; plural=(n != 1);"
