903908bc2d218f7d762c6d17185b09fc79a28a15
Avinash Gusain <avinashg@e.email>
2022-08-18 16:04:12 +0530

    line break added

--- apps/dav/l10n/de.js
+++ apps/dav/l10n/de.js
@@ -2,9 +2,9 @@ OC.L10N.register(
     "dav",
     {
         "Maybe": "Vielleicht",
-        "This occurrence has been updated, please review highlighted information below:":"Dieses Ereignis wurde aktualisiert, bitte überprüfen Sie die hervorgehobenen Informationen unten:",
-       "This event has been updated, please review the information below:":"Diese Veranstaltung wurde aktualisiert, bitte beachten Sie die unten stehenden Informationen:",
-       "This recurring event has been updated, please review the information below:":"Diese wiederkehrende Veranstaltung wurde aktualisiert, bitte beachten Sie die unten stehenden Informationen:",
+        "This occurrence has been updated, please review highlighted information below:":"Dieses Ereignis wurde aktualisiert,<br> bitte überprüfen Sie die hervorgehobenen Informationen unten:",
+       "This event has been updated, please review the information below:":"Diese Veranstaltung wurde aktualisiert,<br> bitte beachten Sie die unten stehenden Informationen:",
+       "This recurring event has been updated, please review the information below:":"Diese wiederkehrende Veranstaltung wurde aktualisiert,<br> bitte beachten Sie die unten stehenden Informationen:",
        "The invitation has been cancelled":"Die Veranstaltung wurde abgesagt"
     },
     "nplurals=2; plural=(n != 1);"
--- apps/dav/l10n/fr.js
+++ apps/dav/l10n/fr.js
@@ -2,9 +2,9 @@ OC.L10N.register(
     "dav",
     {
         "Maybe": "Peut-être",
-        "This occurrence has been updated, please review highlighted information below:":"Cette occurrence a été mise à jour, les éléments modifiés sont mis en évidence ci-dessous :",
-        "This event has been updated, please review the information below:":"Cet événement a été mis à jour, les détails se trouvent ci-dessous :",
-        "This recurring event has been updated, please review the information below:":"Cet événement récurrent a été mis à jour, les détails se trouvent ci-dessous :",
+        "This occurrence has been updated, please review highlighted information below:":"Cette occurrence a été mise à jour,<br> les éléments modifiés sont mis en évidence ci-dessous :",
+        "This event has been updated, please review the information below:":"Cet événement a été mis à jour,<br> les détails se trouvent ci-dessous :",
+        "This recurring event has been updated, please review the information below:":"Cet événement récurrent a été mis à jour,<br> les détails se trouvent ci-dessous :",
         "The invitation has been cancelled":"Cet événément a été annulé"
     },
     "nplurals=2; plural=(n != 1);"
