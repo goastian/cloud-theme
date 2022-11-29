021731513979c4f83397999f5c3c37f0e28acba2
Ronak Patel <ronak.patel@e.email>
2022-11-29 13:34:19 +0530

    user_ldap translations added

---
+++ apps/user_ldap/l10n/it.js
@@ -0,0 +1,6 @@
+OC.L10N.register(
+    "user_ldap",
+    {
+		"This directory is unavailable, please check the logs or contact the administrator": "Questa directory non è disponibile, controllare i log o contattare Murena all'indirizzo <a href=\"mailto:support@helpdesk.com\">support@helpdesk.com</a>."
+},
+"nplurals=2; plural=(n != 1);");
