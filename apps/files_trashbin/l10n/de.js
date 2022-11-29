a12e7bd9c2bca4eaf96e29f9f5e8e1721156d343
Ronak Patel <ronak.patel@e.email>
2022-11-29 12:44:50 +0530

    files_trashbin translations added

---
+++ apps/files_trashbin/l10n/de.js
@@ -0,0 +1,6 @@
+OC.L10N.register(
+    "core",
+    {
+        "This directory is unavailable, please check the logs or contact the administrator": "Dieses Verzeichnis ist nicht verfügbar, bitte überprüfen Sie die Protokolle oder kontaktieren Sie Murena unter <a href=\"mailto:support@helpdesk.com\">support@helpdesk.com</a>."
+},
+"nplurals=2; plural=(n != 1);");
---
+++ apps/files_trashbin/l10n/es.js
@@ -0,0 +1,6 @@
+OC.L10N.register(
+    "core",
+    {
+		"This directory is unavailable, please check the logs or contact the administrator": "Este directorio no está disponible, por favor compruebe los registros o contacte con Murena en <a href=\"mailto:support@helpdesk.com\">support@helpdesk.com</a>."
+},
+"nplurals=2; plural=(n != 1);");
