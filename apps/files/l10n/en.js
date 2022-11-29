d79448b1f78e4a2e1b57b9bdda3b04d52b107004
Ronak Patel <ronak.patel@e.email>
2022-11-29 12:46:22 +0530

    files translations added

---
+++ apps/files/l10n/en.js
@@ -0,0 +1,6 @@
+OC.L10N.register(
+    "core",
+    {
+		"This directory is unavailable, please check the logs or contact the administrator": "This directory is unavailable, please check the logs or contact Murena at <a href=\"mailto:support@helpdesk.com\">support@helpdesk.com</a>."
+	},
+"nplurals=2; plural=(n != 1);");
---
+++ apps/files/l10n/fr.js
@@ -0,0 +1,6 @@
+OC.L10N.register(
+	"core",
+    {
+		"This directory is unavailable, please check the logs or contact the administrator": "Ce répertoire est indisponible, veuillez vérifier les journaux ou contacter Murena à <a href=\"mailto:support@helpdesk.com\">support@helpdesk.com</a>."
+},
+"nplurals=2; plural=(n != 1);");
