2d043287a2a30caa795ed97d20715436b1970d95
Avinash Gusain <avinashg@e.email>
2022-12-05 18:36:52 +0530

    reset password message correction

--- core/l10n/fr.js
+++ core/l10n/fr.js
@@ -14,6 +14,7 @@ OC.L10N.register(
 		"All Rights Reserved": "Tous les droits sont réservés",
 		"Copyright": "droits d'auteur",
 		"Don't have an account yet?": "Vous n'avez pas encore de compte ?",
-		"Create an account": "Créer un compte"
+		"Create an account": "Créer un compte",
+		"A password reset message has been sent to the email address of this account. If you do not receive it, check your spam/junk folders or ask your local administrator for help." : "Un message de réinitialisation du mot de passe a été envoyé à l'<b>adresse électronique de récupération</b> de ce compte."
 },
 "nplurals=2; plural=(n != 1);");
