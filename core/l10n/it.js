8877fc97fe38b1728e981823b934e0fa2e13ab8c
Avinash Gusain <avinashg@e.email>
2022-12-08 13:21:36 +0530

    italian added

--- core/l10n/it.js
+++ core/l10n/it.js
@@ -1,20 +1,6 @@
 OC.L10N.register(
     "core",
     {
-		"Get your FREE Murena account now" : "Get your FREE Murena account now",
-		"1GB storage for FREE to store and sync your pictures & videos.": "1GB storage for FREE to store and sync your pictures & videos.",
-		"Edit your documents online.": "Edit your documents online.",
-		"Your unique email address @murena.io": "Your unique email address @murena.io",
-		"Sync calendar and contacts with the cloud": "Sync calendar and contacts with the cloud",
-		"and many new features added regularly!": "and many new features added regularly!",
-		"Create My Account": "Create My Account",
-		"Sign in to your account": "Sign in to your account",
-		"Terms of Service": "Terms of Service",
-		"Privacy Policy": "Privacy Policy",
-		"All Rights Reserved": "All Rights Reserved",
-		"Copyright": "Copyright",
-		"Don't have an account yet?": "Don't have an account yet?",
-		"Create an account": "Create an account",
 		"A password reset message has been sent to the email address of this account. If you do not receive it, check your spam/junk folders or ask your local administrator for help." : "È stato inviato un messaggio di reimpostazione della password all'indirizzo e-mail di recupero di questo account.",
 		"If it is not there ask your local administrator.": ""
 },
