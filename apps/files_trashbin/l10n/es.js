a362fc92562cb1c89635da938062395bb383d142
Avinash Gusain <avinashg@e.email>
2023-08-07 13:37:52 +0530

    changes Murena to Murena Cloud

--- apps/files_trashbin/l10n/es.js
+++ apps/files_trashbin/l10n/es.js
@@ -1,6 +1,6 @@
 OC.L10N.register(
     "files_trashbin",
     {
-		"This directory is unavailable, please check the logs or contact the administrator": "Este directorio no está disponible, por favor compruebe los registros o contacte con Murena en helpdesk@murena.com."
+		"This directory is unavailable, please check the logs or contact the administrator": "Este directorio no está disponible, por favor compruebe los registros o contacte con Murena Cloud en helpdesk@murena.com."
 },
 "nplurals=2; plural=(n != 1);");
