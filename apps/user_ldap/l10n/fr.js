ea05ea17a02ab5f3fed2935b9a301569da168c8c
Avinash Gusain <avinashg@e.email>
2023-08-08 13:43:33 +0530

    Murena cloud

--- apps/user_ldap/l10n/fr.js
+++ apps/user_ldap/l10n/fr.js
@@ -1,7 +1,7 @@
 OC.L10N.register(
 	"user_ldap",
     {
-		"Please try again or contact your administrator." : "Veuillez réessayer ou contacter Murena à helpdesk@murena.com.",
-		"<b>Warning:</b> The PHP LDAP module is not installed, the backend will not work. Please ask your system administrator to install it." : "<b>Avertissement:</b> Le module LDAP de PHP n'est pas installé, le backend ne fonctionnera pas. Veuillez contacter Murena à helpdesk@murena.com pour l'installer."
+		"Please try again or contact your administrator." : "Veuillez réessayer ou contacter Murena Cloud à helpdesk@murena.com.",
+		"<b>Warning:</b> The PHP LDAP module is not installed, the backend will not work. Please ask your system administrator to install it." : "<b>Avertissement:</b> Le module LDAP de PHP n'est pas installé, le backend ne fonctionnera pas. Veuillez contacter Murena Cloud à helpdesk@murena.com pour l'installer."
 },
 "nplurals=2; plural=(n != 1);");
--- apps/user_ldap/l10n/it.js
+++ apps/user_ldap/l10n/it.js
@@ -1,7 +1,7 @@
 OC.L10N.register(
     "user_ldap",
     {
-		"Please try again or contact your administrator." : "Riprovare o contattare Murena all'indirizzo helpdesk@murena.com.",
-		"<b>Warning:</b> The PHP LDAP module is not installed, the backend will not work. Please ask your system administrator to install it." : "<b>Avvertenze:</b> Se il modulo PHP LDAP non è installato, il backend non funziona. Per installarlo, contattare Murena all'indirizzo helpdesk@murena.com."
+		"Please try again or contact your administrator." : "Riprovare o contattare Murena Cloud all'indirizzo helpdesk@murena.com.",
+		"<b>Warning:</b> The PHP LDAP module is not installed, the backend will not work. Please ask your system administrator to install it." : "<b>Avvertenze:</b> Se il modulo PHP LDAP non è installato, il backend non funziona. Per installarlo, contattare Murena Cloud all'indirizzo helpdesk@murena.com."
 },
 "nplurals=2; plural=(n != 1);");
