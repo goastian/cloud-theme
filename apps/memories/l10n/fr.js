1a4bad9f604a3041602eed9004569a7def74e38d
Akhil <akhil.potukuchi@gmail.com>
2023-09-15 09:21:31 +0000

    Rename memories to Gallery

---
+++ apps/memories/l10n/fr.js
@@ -0,0 +1,8 @@
+OC.L10N.register(
+    "memories",
+    {
+        "Gallery": "Galerie",
+        "Gallery Settings": "Paramètres de Galerie",
+        "Gallery has been updated to {version}. Reload to get the new version.": "Galerie a été mise à jour vers la version {version}. Rechargez la page pour bénéficier de la nouvelle version.",
+    },
+    "nplurals=2; plural=(n != 1);");
