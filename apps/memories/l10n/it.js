c517a68e7cd332ce62f3582f03d78f891c06bd47
Akhil <akhil@e.email>
2023-09-15 14:50:49 +0530

    Fix formatting

--- apps/memories/l10n/it.js
+++ apps/memories/l10n/it.js
@@ -1,8 +1,8 @@
 OC.L10N.register(
     "memories",
     {
-        "Gallery": "Galleria", 
+        "Gallery": "Galleria",
         "Gallery Settings": "Impostazioni della galleria",
-        "Gallery has been updated to {version}. Reload to get the new version." : "Abbiamo aggiornato Galleria alla nuova versione (version). Per poterla utilizzare, ricarica la pagina.",
+        "Gallery has been updated to {version}. Reload to get the new version.": "Abbiamo aggiornato Galleria alla nuova versione (version). Per poterla utilizzare, ricarica la pagina.",
     },
     "nplurals=2; plural=(n != 1);");
