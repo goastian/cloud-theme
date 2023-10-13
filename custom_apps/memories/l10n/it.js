cd2b4969551bc2f7aa416f3f6ba51636a9cab2ca
Akhil <akhil.potukuchi@gmail.com>
2023-10-13 06:38:18 +0000

    Move js translations into core folder

---
+++ custom_apps/memories/l10n/it.js
@@ -0,0 +1,5 @@
+OC.L10N.register("memories", {
+	"Gallery": "Galleria",
+	"Gallery Settings": "Impostazioni Galleria",
+	"Gallery has been updated to {version}. Reload to get the new version.": "Abbiamo aggiornato Galleria alla nuova versione (version). Per poterla utilizzare, ricarica la pagina.",
+}, "nplurals=2; plural=(n != 1);")
