5bd399d356471936e6d85e8df1e219b86d0444de
Akhil <akhil@e.email>
2024-01-03 16:59:09 +0530

    Change Nextcloud to Murena Cloud where needed

--- custom_apps/memories/l10n/fr.js
+++ custom_apps/memories/l10n/fr.js
@@ -3,4 +3,5 @@ OC.L10N.register("memories",
 		"Gallery": "Galerie",
 		"Gallery Settings": "Paramètres de Galerie",
 		"Gallery has been updated to {version}. Reload to get the new version.": "Galerie a été mise à jour vers la version {version}. Rechargez la page pour bénéficier de la nouvelle version.",
+		"Public link shares are available to people outside Nextcloud." : "Les partages par lien public sont accessibles aux personnes extérieures à Murena Cloud.",
 	}, "nplurals=2; plural=(n != 1);")
--- custom_apps/memories/l10n/it.js
+++ custom_apps/memories/l10n/it.js
@@ -2,4 +2,5 @@ OC.L10N.register("memories", {
 	"Gallery": "Galleria",
 	"Gallery Settings": "Impostazioni Galleria",
 	"Gallery has been updated to {version}. Reload to get the new version.": "Abbiamo aggiornato Galleria alla nuova versione (version). Per poterla utilizzare, ricarica la pagina.",
+	"Public link shares are available to people outside Nextcloud." : "Le condivisioni di link pubblici sono disponibili a persone esterne a Murena Cloud.",
 }, "nplurals=2; plural=(n != 1);")
