f2fec8d847a3276f148d87a4ddd4d64efb941300
Akhil <akhil@e.email>
2024-01-16 14:42:20 +0530

    Italian translation corrected

--- custom_apps/memories/l10n/it.js
+++ custom_apps/memories/l10n/it.js
@@ -3,5 +3,5 @@ OC.L10N.register("memories", {
 	"Gallery Settings": "Impostazioni Galleria",
 	"Gallery has been updated to {version}. Reload to get the new version.": "Abbiamo aggiornato Galleria alla nuova versione (version). Per poterla utilizzare, ricarica la pagina.",
 	"Public link shares are available to people outside Nextcloud." : "Le condivisioni di link pubblici sono disponibili a persone esterne a Murena Cloud.",
-	"Share an external Nextcloud link": "Condividi un Murena Cloud link esterno"
+	"Share an external Nextcloud link": "Condividi un link Murena Cloud all'esterno"
 }, "nplurals=2; plural=(n != 1);")
