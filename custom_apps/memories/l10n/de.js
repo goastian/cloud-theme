a95d175cb4becf612a1072939d433873ef5318c0
Akhil <akhil.potukuchi@gmail.com>
2024-01-16 08:45:36 +0000

    Fix reference to Nextcloud with sharing links

--- custom_apps/memories/l10n/de.js
+++ custom_apps/memories/l10n/de.js
@@ -3,4 +3,5 @@ OC.L10N.register("memories", {
 	"Gallery Settings": "Bildergalerie-Einstellungen",
 	"Gallery has been updated to {version}. Reload to get the new version.": "Bildergalerie wurde auf {version} aktualisiert. Für die neue Version bitte neu laden.",
 	"Public link shares are available to people outside Nextcloud." : "Öffentliche Linkfreigaben sind für Personen außerhalb Ihrer Murena Cloud verfügbar.",
+	"Share an external Nextcloud link" : "Einen externen Murena Cloud-Link teilen",
 }, "nplurals=2; plural=(n != 1);")
