abc5d21cf6cfd83fc1b4148126b2687a74eb96b2
Paolo Pantaleo <panta@e.email>
2024-01-24 21:48:19 +0000

    Translated using Weblate (Italian)
    Currently translated at 100.0% (5 of 5 strings)
    
    Translation: Murena Cloud Theme/Dav
    Translate-URL: https://i18n.e.foundation/projects/murena-cloud-theme/dav/it/

--- apps/dav/l10n/it.js
+++ apps/dav/l10n/it.js
@@ -2,7 +2,7 @@ OC.L10N.register(
     "dav",
     {
     "Maybe" : "Forse",
-    "This occurrence has been updated,<br> please review highlighted information below:" : "Questa ricorrenza è stata aggiornata,<br> controlla le informazioni evidenziate di seguito:",
+    "This occurrence has been updated,<br> please review highlighted information below:" : "Questa occorrenza è stata aggiornata,<br> controlla le informazioni evidenziate di seguito:",
     "This event has been updated,<br> please review the information below:" : "Questo evento è stato aggiornato,<br> controlla le informazioni di seguito:",
     "This recurring event has been updated,<br> please review the information below:" : "L' evento ricorrente è stato aggiornato,<br> verifica l'informazione seguente:",
     "The event has been cancelled" : "L'evento è stato cancellato"
