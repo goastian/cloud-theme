13f762cc0a183ad7f3619de7acbcf85d56c8d4bf
Kristian Nordin <kristian.po@proton.me>
2024-01-30 21:54:03 +0000

    Translated using Weblate (Swedish)
    Currently translated at 100.0% (1 of 1 strings)
    
    Translation: Murena Cloud Theme/Files Trashbin
    Translate-URL: https://i18n.e.foundation/projects/murena-cloud-theme/files-trashbin/sv/

---
+++ apps/files_trashbin/l10n/sv.js
@@ -0,0 +1,6 @@
+OC.L10N.register(
+    "files_trashbin",
+    {
+    "This directory is unavailable, please check the logs or contact Murena Cloud at helpdesk@murena.com." : "Denna katalog är inte tillgänglig. Kontrollera loggen eller kontakta Murena Cloud via helpdesk@murena.com."
+},
+"nplurals=2; plural=n != 1;");
