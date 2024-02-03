28ad2821a2f62de8cc6d07a41c991540ef035bf1
Kristian Nordin <kristian.po@proton.me>
2024-02-03 22:28:46 +0000

    Translated using Weblate (Swedish)
    Currently translated at 50.0% (3 of 6 strings)
    
    Translation: Murena Cloud Theme/Privacy
    Translate-URL: https://i18n.e.foundation/projects/murena-cloud-theme/privacy/sv/

---
+++ apps/privacy/l10n/sv.js
@@ -0,0 +1,8 @@
+OC.L10N.register(
+    "privacy",
+    {
+    "Learn more about this topic here." : "Lär dig mer om ämnet här.",
+    "Where is my data?" : "Vart är min data?",
+    "Who has access to my data?" : "Vem har tillgång till min data?"
+},
+"nplurals=2; plural=n != 1;");
