e6de4c31bf0f9b8d838f407d0d2e50af5251bcce
Kristian Nordin <kristian.po@proton.me>
2024-02-13 20:11:50 +0000

    Translated using Weblate (Swedish)
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: Murena Cloud Theme/Privacy
    Translate-URL: https://i18n.e.foundation/projects/murena-cloud-theme/privacy/sv/

--- apps/privacy/l10n/sv.js
+++ apps/privacy/l10n/sv.js
@@ -1,7 +1,10 @@
 OC.L10N.register(
     "privacy",
     {
+    "Like in most cloud services, a reduced number of administrators can see your files and all the information in the database as they need to make backups, perform upgrades, reset passwords, etc." : "Likt andra molntjänster kan ett begränsat antal administratörer se dina filer och all information i databasen eftersom dom behöver göra säkerhetskopior, installera uppgraderingar, återställa lösenord, m.m.",
     "Learn more about this topic here." : "Lär dig mer om ämnet här.",
+    "To protect your data, we have implemented server-side-encryption on our servers which hides by default the content of your files and notes even to these administrators." : "För att skydda din data har vi implementerat kryptering på serversidan vilket gömmer innehållet i dina filer och anteckningar som standard för dessa administratörer.",
+    "People you shared content with" : "Personer du delat innehåll med",
     "Where is my data?" : "Vart är min data?",
     "Who has access to my data?" : "Vem har tillgång till min data?"
 },
