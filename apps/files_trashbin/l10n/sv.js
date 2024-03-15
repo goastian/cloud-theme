59ba993e9a95ca8eb40ca04d7133a1cdf7e251e7
Akhil <akhil.potukuchi@gmail.com>
2024-03-15 12:27:33 +0000

    Undo translation source string issues

--- apps/files_trashbin/l10n/sv.js
+++ apps/files_trashbin/l10n/sv.js
@@ -1,6 +1,6 @@
 OC.L10N.register(
     "files_trashbin",
     {
-    "This directory is unavailable, please check the logs or contact Murena Cloud at helpdesk@murena.com." : "Denna katalog är inte tillgänglig. Kontrollera loggen eller kontakta Murena Cloud via helpdesk@murena.com."
+    "This directory is unavailable, please check the logs or contact the administrator" : "Denna katalog är inte tillgänglig. Kontrollera loggen eller kontakta Murena Cloud via helpdesk@murena.com."
 },
 "nplurals=2; plural=n != 1;");
--- apps/privacy/l10n/it.js
+++ apps/privacy/l10n/it.js
@@ -1,11 +1,11 @@
 OC.L10N.register(
     "privacy",
     {
-    "Like in most cloud services, a reduced number of administrators can see your files and all the information in the database as they need to make backups, perform upgrades, reset passwords, etc." : "Come con la maggior parte dei servizi cloud, un piccolo numero di amministratori può vedere i file e tutte le informazioni nel database; in questo modo possono fare backup, eseguire aggiornamenti o resettare le password, tra le altre operazioni.",
-    "Learn more about this topic here." : "Per saperne di più, leggete qui.",
-    "To protect your data, we have implemented server-side-encryption on our servers which hides by default the content of your files and notes even to these administrators." : "Per proteggere i tuoi dati, abbiamo abilitato la crittografia sul server che impedisce anche agli amministratori di visualizzare il contenuto dei file e delle note per impostazione predefinita.",
-    "People you shared content with" : "Persone con cui hai condiviso contenuti",
-    "Where is my data?" : "Dove sono conservati i miei dati?",
-    "Who has access to my data?" : "Chi può accedere ai miei dati?"
-},
-"");
+        "Like in most cloud services, a reduced number of administrators can see your files and all the information in the database as they need to make backups, perform upgrades, reset passwords, etc.": "Come con la maggior parte dei servizi cloud, un piccolo numero di amministratori può vedere i file e tutte le informazioni nel database; in questo modo possono fare backup, eseguire aggiornamenti o resettare le password, tra le altre operazioni.",
+        "Learn more about this topic here.": "Per saperne di più, leggete qui.",
+        "To protect your data, we have implemented server-side-encryption on our servers which hides by default the content of your files and notes even to these administrators.": "Per proteggere i tuoi dati, abbiamo abilitato la crittografia sul server che impedisce anche agli amministratori di visualizzare il contenuto dei file e delle note per impostazione predefinita.",
+        "People you shared documents or folders with": "Persone con cui hai condiviso contenuti",
+        "Where is your data?": "Dove sono conservati i miei dati?",
+        "Who has access to your data?": "Chi può accedere ai miei dati?"
+    },
+    "");
