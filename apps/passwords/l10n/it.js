0ace7ec64b6ea086636d0b6717d85258f0011f84
Phie <phie@phie.ovh>
2024-06-18 17:01:40 +0200

    new translations for es de it

---
+++ apps/passwords/l10n/it.js
@@ -0,0 +1,11 @@
+OC.L10N.register(
+    "passwords",
+    {
+        "Your Browser is outdated": "Il browser è obsoleto",
+        "Your browser is outdated and does not provide the necessary functionality to display this page.": "Il vostro browser è obsoleto e non fornisce le funzionalità necessarie per visualizzare questa pagina.",
+        "To view this website properly, please enable Javascript JIT.": "Per visualizzare correttamente questo sito web, abilitare Javascript JIT.",
+        "How to enable it": "Come attivarlo",
+        "Please check if an update is available for your browser or choose a modern and compatible browser from the list below.": "Verificate se è disponibile un aggiornamento per il vostro browser o scegliete un browser moderno e compatibile dall'elenco seguente.",
+        "JavaScript JIT might be disabled in your browser in order to render web content in a more secure configuration. You can always enable Javascript JIT in the settings, depending on your web browser.": "JavaScript JIT is disabled by default in the browser in order to render web content in a more secure configuration. È sempre possibile attivare Javascript JIT nelle impostazioni. <br /><br /><p><b>Passi per il browser eOS:</b> Fare clic sull'icona con i tre puntini in alto a sinistra e andare su:<div class=\"quote\">Impostazioni > Impostazioni del sito</div>Quindi, attivate <b>Javascript JIT</b> in fondo all'elenco.</p>"
+   },
+"nplurals=2; plural=(n != 1);");
