5a0404b5731b9a79d733af0442f02b7fe1de4d9e
Akhil <akhil.potukuchi@gmail.com>
2024-07-10 15:37:10 +0530

    Change murena cloud to murena workspace everywhere

--- apps/user_ldap/l10n/en.js
+++ apps/user_ldap/l10n/en.js
@@ -1,7 +1,7 @@
 OC.L10N.register(
     "user_ldap",
     {
-		"Please try again or contact your administrator." : "Please try again or contact Murena Cloud at helpdesk@murena.com.",
-		"<b>Warning:</b> The PHP LDAP module is not installed, the backend will not work. Please ask your system administrator to install it." : "<b>Warning:</b> The PHP LDAP module is not installed, the backend will not work. Please contact Murena Cloud at helpdesk@murena.com to install it."
+		"Please try again or contact your administrator." : "Please try again or contact Murena Workspace at helpdesk@murena.com.",
+		"<b>Warning:</b> The PHP LDAP module is not installed, the backend will not work. Please ask your system administrator to install it." : "<b>Warning:</b> The PHP LDAP module is not installed, the backend will not work. Please contact Murena Workspace at helpdesk@murena.com to install it."
 	},
 "nplurals=2; plural=(n != 1);");
--- custom_apps/memories/l10n/fr.js
+++ custom_apps/memories/l10n/fr.js
@@ -3,6 +3,6 @@ OC.L10N.register("memories",
 		"Gallery": "Galerie",
 		"Gallery Settings": "Paramètres de Galerie",
 		"Gallery has been updated to {version}. Reload to get the new version.": "Galerie a été mise à jour vers la version {version}. Rechargez la page pour bénéficier de la nouvelle version.",
-		"Public link shares are available to people outside Nextcloud." : "Les partages par lien public sont accessibles aux personnes extérieures à Murena Cloud.",
-		"Share an external Nextcloud link" : "Partager un lien Murena Cloud externe",
+		"Public link shares are available to people outside Nextcloud." : "Les partages par lien public sont accessibles aux personnes extérieures à Murena Workspace.",
+		"Share an external Nextcloud link" : "Partager un lien Murena Workspace externe",
 	}, "nplurals=2; plural=(n != 1);")
