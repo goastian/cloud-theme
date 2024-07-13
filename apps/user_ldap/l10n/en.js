6685451d0e9a88f6de7c1cf3cb4291d383e465f5
Kristian Nordin <kristian.po@proton.me>
2024-07-13 19:57:51 +0000

    Translated using Weblate (Swedish)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: Murena Cloud Theme/User Ldap
    Translate-URL: https://i18n.e.foundation/projects/murena-cloud-theme/user-ldap/sv/

--- apps/user_ldap/l10n/en.js
+++ apps/user_ldap/l10n/en.js
@@ -1,7 +1,7 @@
 OC.L10N.register(
     "user_ldap",
     {
-		"Please try again or contact your administrator." : "Please try again or contact Murena Cloud at helpdesk@murena.com.",
-		"<b>Warning:</b> The PHP LDAP module is not installed, the backend will not work. Please ask your system administrator to install it." : "<b>Warning:</b> The PHP LDAP module is not installed, the backend will not work. Please contact Murena Cloud at helpdesk@murena.com to install it."
-	},
-"nplurals=2; plural=(n != 1);");
+    "Please try again or contact Murena Cloud at helpdesk@murena.com." : "Please try again or contact Murena Cloud at helpdesk@murena.com.",
+    "<b>Warning:</b> The PHP LDAP module is not installed, the backend will not work. Please contact Murena Cloud at helpdesk@murena.com to install it." : "<b>Warning:</b> The PHP LDAP module is not installed, the backend will not work. Please contact Murena Cloud at helpdesk@murena.com to install it."
+},
+"");
--- apps/user_ldap/l10n/es.js
+++ apps/user_ldap/l10n/es.js
@@ -1,7 +1,7 @@
 OC.L10N.register(
     "user_ldap",
     {
-		"Please try again or contact your administrator.": "Por favor, inténtelo de nuevo o contacte con Murena Cloud en helpdesk@murena.com.",
-		"<b>Warning:</b> The PHP LDAP module is not installed, the backend will not work. Please ask your system administrator to install it.": "<b>Advertencia:</b> El módulo LDAP de PHP no está instalado, el backend no funcionará. Póngase en contacto con Murena Cloud en helpdesk@murena.com para instalarlo."
+    "Please try again or contact Murena Cloud at helpdesk@murena.com." : "Por favor, inténtelo de nuevo o contacte con Murena Cloud en helpdesk@murena.com.",
+    "<b>Warning:</b> The PHP LDAP module is not installed, the backend will not work. Please contact Murena Cloud at helpdesk@murena.com to install it." : "<b>Advertencia:</b> El módulo LDAP de PHP no está instalado, el backend no funcionará. Póngase en contacto con Murena Cloud en helpdesk@murena.com para instalarlo."
 },
-"nplurals=2; plural=(n != 1);");
+"");
