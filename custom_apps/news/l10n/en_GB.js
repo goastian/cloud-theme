bdff180bcfee251fed3033581ad7b29a7318b6ee
Avinash Gusain <avinashg@e.email>
2024-08-27 10:34:37 +0530

    added translation for news

---
+++ custom_apps/news/l10n/en_GB.js
@@ -0,0 +1,7 @@
+OC.L10N.register(
+    "news",
+    {
+        "Internal server error! Please check your data/nextcloud.log file for additional information!": "Internal server error! Please check with support for additional information!",
+        "Request failed, Nextcloud is in currently in maintenance mode!": "Request failed, Murena is in currently in maintenance mode!"
+    },
+    "nplurals=2; plural=(n != 1);");
---
+++ custom_apps/news/l10n/it.js
@@ -0,0 +1,7 @@
+OC.L10N.register(
+    "news",
+    {
+        "Internal server error! Please check your data/nextcloud.log file for additional information!": "Errore interno del server! Si prega di verificare con il supporto per ulteriori informazioni!",
+        "Request failed, Nextcloud is in currently in maintenance mode!": "Richiesta non riuscita, Murena è attualmente in manutenzione!"
+    },
+    "nplurals=2; plural=(n != 1);");
