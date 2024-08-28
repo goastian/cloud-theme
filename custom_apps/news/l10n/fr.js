9e376c762651649209dc5a6adcf7bdc6229cd9a5
AVINASH GUSAIN <avinashg@e.email>
2024-08-28 13:16:18 +0000

    added translation for news

---
+++ custom_apps/news/l10n/fr.js
@@ -0,0 +1,7 @@
+OC.L10N.register(
+    "news",
+    {
+        "Internal server error! Please check your data/nextcloud.log file for additional information!": "Erreur de serveur interne ! Veuillez contacter le support pour plus d'informations !",
+        "Request failed, Nextcloud is in currently in maintenance mode!": "La requête a échoué, Murena Workspace est actuellement en maintenance !"
+    },
+    "nplurals=2; plural=(n != 1);");
