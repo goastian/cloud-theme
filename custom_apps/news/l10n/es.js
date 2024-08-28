faa0d489bd08a0dee2aca9e2d63d9062adb85b0d
Avinash Gusain <avinashg@e.email>
2024-08-28 11:17:29 +0530

    change Murena to Murena Workspace

--- custom_apps/news/l10n/es.js
+++ custom_apps/news/l10n/es.js
@@ -2,6 +2,6 @@ OC.L10N.register(
     "news",
     {
         "Internal server error! Please check your data/nextcloud.log file for additional information!": "Error interno del servidor. Por favor, consulte con el soporte para obtener información adicional.",
-        "Request failed, Nextcloud is in currently in maintenance mode!": "Petición fallida! Murena está actualmente en modo mantenimiento."
+        "Request failed, Nextcloud is in currently in maintenance mode!": "Petición fallida! Murena Workspace está actualmente en modo mantenimiento."
     },
     "nplurals=2; plural=(n != 1);");
